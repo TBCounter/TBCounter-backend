715ab3f00f036bb07778776dbc1233e8f254603f
Ivan Riazanov <omega19961@gmail.com>
2024-06-30 16:52:45 +0600

    connect to specific DB

--- storage/index.js
+++ storage/index.js
@@ -6,7 +6,7 @@ async function connect_storage() {
     console.log(process.env.MONGO_USER)
     console.log(process.env.MONGO_PASSWD)
     console.log(process.env.MONGO_URL)
-    return mongoose.connect(`mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASSWD}@${process.env.MONGO_URL}`)
+    return mongoose.connect(`mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASSWD}@${process.env.MONGO_URL}`, { dbName: 'chests' })
 }
 
 
