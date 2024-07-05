6345988969f5fef4d1e4e875f6b8b22301a2e88c
normalcore <normalcorecore@gmail.com>
2024-07-05 18:32:42 +0600

    created route for uploading aws link

--- routes/database.js
+++ routes/database.js
@@ -4,9 +4,10 @@ var path = require('path')
 const { Chest } = require('../storage')
 
 router.post('/', async function (req, res) {
-  let { chestname } = await req.body
+  // create account id link to chests
+  //let { accountId } = await req.body
+
   const chestId = await Chest.create({
-    name: chestname,
     status: "CREATED",
     got_at: new Date()
 })
@@ -16,10 +17,19 @@ router.post('/', async function (req, res) {
   // Записать ссылку на скачивание изображения в chest в mongo (url поле в монго)
   // res.status(200).json( { upliadLink: https://s3.aws.link/asdau32ud9 })
 
-
   // Нода получает ссылку на загрузку. Загружает изображение и по вебсокетам меняет статус сундука на uploaded
 
   res.status(200).json(chestId.id)
 });
+
+router.post('/url', async function (req, res) {
+  let { url } = await req.body
+
+  let chestid = url.split('_')[1].split('.')[0]
+
+  await Chest.findByIdAndUpdate(chestid, {url: url, status: 'UPLOADED'})
+  res.status(200).json({ uploadLink: url })
+});
+
 
 module.exports = router;
