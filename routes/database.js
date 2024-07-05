85cc1b80d598b1046c3895515b324564f57571a2
normalcore <normalcorecore@gmail.com>
2024-07-05 21:04:14 +0600

    reworked aws url logic

--- routes/database.js
+++ routes/database.js
@@ -1,35 +1,61 @@
 var express = require('express');
 var router = express.Router();
 var path = require('path')
+
 const { Chest } = require('../storage')
+const AWS = require('aws-sdk');
+
+const accessKeyId = process.env.S3_ACCESS_KEY;
+const secretAccessKey = process.env.S3_SECRET_KEY;
+const region = 'eu-north-1';
+const bucketName = 'tbcounter-screenshots';
+
+AWS.config.update({
+  accessKeyId: accessKeyId,
+  secretAccessKey: secretAccessKey,
+  region: region
+});
+
+const s3 = new AWS.S3();
 
 router.post('/', async function (req, res) {
   // create account id link to chests
-  //let { accountId } = await req.body
-
+  // let { accountId } = await req.body
+  
   const chestId = await Chest.create({
     status: "CREATED",
     got_at: new Date()
-})
+  })
 
+  let url = await getSignedUrl(chestId.id)
 
+  await Chest.findByIdAndUpdate(chestId, {url: url})
   // Идти на AWS, получить ссылку для загрузки изрображения для файла с именем chestId.id
   // Записать ссылку на скачивание изображения в chest в mongo (url поле в монго)
   // res.status(200).json( { upliadLink: https://s3.aws.link/asdau32ud9 })
 
   // Нода получает ссылку на загрузку. Загружает изображение и по вебсокетам меняет статус сундука на uploaded
 
-  res.status(200).json(chestId.id)
+  res.status(200).json({ uploadLink: url, chestId: chestId.id })
 });
 
-router.post('/url', async function (req, res) {
-  let { url } = await req.body
+function getSignedUrl(fileName) {
+  return new Promise((resolve, reject) => {
+      const params = {
+          Bucket: bucketName,
+          Key: fileName,
+          Expires: 60
+      };
 
-  let chestid = url.split('_')[1].split('.')[0]
-
-  await Chest.findByIdAndUpdate(chestid, {url: url, status: 'UPLOADED'})
-  res.status(200).json({ uploadLink: url })
-});
-
+      s3.getSignedUrl('putObject', params, (err, url) => {
+          if (err) {
+              console.error('Ошибка при генерации подписанного URL', err);
+              reject(err);
+          } else {
+              resolve(url);
+          }
+      });
+  });
+}
 
 module.exports = router;
