82c449580998cb93c01cb134644f3a094e64091e
Ivan Riazanov <omega19961@gmail.com>
2024-08-25 14:33:06 +0600

    add uuid for users and accounts

--- db/models/account.js
+++ db/models/account.js
@@ -1,44 +1,50 @@
 module.exports = (sequelize, Sequelize) => {
   const Account = sequelize.define("account", {
-      login: {
-        type: Sequelize.STRING,
-        allowNull: true
-      },
-      password: {
-        type: Sequelize.STRING,
-        allowNull: true
-      },
-      isTriumph: {
-        type: Sequelize.BOOLEAN
-      },
+    id: {
+      type: Sequelize.UUID,
+      defaultValue: Sequelize.UUIDV4,
+      allowNull: false,
+      primaryKey: true
+    },
+    login: {
+      type: Sequelize.STRING,
+      allowNull: true
+    },
+    password: {
+      type: Sequelize.STRING,
+      allowNull: true
+    },
+    isTriumph: {
+      type: Sequelize.BOOLEAN
+    },
 
-      name: {
-        type: Sequelize.STRING,
-        allowNull: false
-      },
-      clan: {
-        type: Sequelize.STRING
-      },
-      avatar: {
-        type: Sequelize.STRING,
-        defaultValue: __dirname + '/images'
-      },
-      is_locked: {
-        type: Sequelize.BOOLEAN
-      },
-
-
-      node_url: {
-        type: Sequelize.STRING
-      },
-      node_url_id: {
-        type: Sequelize.STRING
-      },
+    name: {
+      type: Sequelize.STRING,
+      allowNull: false
+    },
+    clan: {
+      type: Sequelize.STRING
+    },
+    avatar: {
+      type: Sequelize.STRING,
+      defaultValue: __dirname + '/images'
+    },
+    is_locked: {
+      type: Sequelize.BOOLEAN
+    },
 
 
-      vip: {
-        type: Sequelize.BOOLEAN
-      },
+    node_url: {
+      type: Sequelize.STRING
+    },
+    node_url_id: {
+      type: Sequelize.STRING
+    },
+
+
+    vip: {
+      type: Sequelize.BOOLEAN
+    },
   });
 
   return Account;
--- db/models/user.js
+++ db/models/user.js
@@ -1,14 +1,20 @@
 module.exports = (sequelize, Sequelize) => {
-    const User = sequelize.define("user", {
-        email: {
-            type: Sequelize.STRING
-        },
-        password: {
-            type: Sequelize.STRING
-        }
-    });
+  const User = sequelize.define("user", {
+    id: {
+      type: Sequelize.UUID,
+      defaultValue: Sequelize.UUIDV4,
+      allowNull: false,
+      primaryKey: true
+    },
+    email: {
+      type: Sequelize.STRING
+    },
+    password: {
+      type: Sequelize.STRING
+    }
+  });
 
-    return User;
+  return User;
 };
 
 //has relationship
