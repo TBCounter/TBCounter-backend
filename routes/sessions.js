1610ec005903d5e4bc3c0456a418786ba60c5279
Ivan Riazanov <omega19961@gmail.com>
2024-10-07 10:51:40 +0600

    sessionns endpoint fix

--- routes/sessions.js
+++ routes/sessions.js
@@ -1,22 +1,73 @@
 var express = require('express');
 var router = express.Router();
 var path = require('path');
-const { Session } = require('../storage');
+const { Session, Chest } = require('../storage');
 const authorization = require('../middleware/authorization');
 
 const db = require('../db')
 
-// image route
 router.get('/', authorization, async function (req, res) {
-  const { accountId } = req.body
-  console.log(accountId)
+  const accountId = req.query.accountId
+  console.log('accid', accountId)
+
+  if (!accountId) {
+    return res.status(401).send('Invalid account')
+  }
 
   const user = await db.accounts.findOne({ where: { id: accountId } })
   if (!user) {
     return res.status(401).send('Invalid account')
   }
 
-  const accSessions = await Session.find({ account_id: accountId })
+  const accSessions = await Session.aggregate([
+    {
+      $match: { account_id: accountId }  // Фильтр по account_id
+    },
+    {
+      $lookup: {
+        from: 'chests',  // Название коллекции сундуков
+        localField: 'session_id',  // Поле session_id в коллекции сессий
+        foreignField: 'session_id',  // Поле session_id в коллекции сундуков
+        as: 'chests'  // Имя поля для объединенных данных
+      }
+    },
+    {
+      $addFields: {
+        chestStatusCounts: {
+          $reduce: {
+            input: "$chests",
+            initialValue: { CREATED: 0, UPLOADED: 0, PROCESSING: 0, PROCESSED: 0, ERROR: 0 },
+            in: {
+              CREATED: {
+                $cond: [{ $eq: ["$$this.status", "CREATED"] }, { $add: ["$$value.CREATED", 1] }, "$$value.CREATED"]
+              },
+              UPLOADED: {
+                $cond: [{ $eq: ["$$this.status", "UPLOADED"] }, { $add: ["$$value.UPLOADED", 1] }, "$$value.UPLOADED"]
+              },
+              PROCESSING: {
+                $cond: [{ $eq: ["$$this.status", "PROCESSING"] }, { $add: ["$$value.PROCESSING", 1] }, "$$value.PROCESSING"]
+              },
+              PROCESSED: {
+                $cond: [{ $eq: ["$$this.status", "PROCESSED"] }, { $add: ["$$value.PROCESSED", 1] }, "$$value.PROCESSED"]
+              },
+              ERROR: {
+                $cond: [{ $eq: ["$$this.status", "ERROR"] }, { $add: ["$$value.ERROR", 1] }, "$$value.ERROR"]
+              }
+            }
+          }
+        }
+      }
+    },
+    {
+      $project: {
+        session_id: 1,  // Выводим session_id
+        start_time: 1,  // Выводим start_time
+        end_time: 1,  // Выводим end_time
+        status: 1,  // Выводим статус сессии
+        chestStatusCounts: 1  // Выводим сгруппированные статусы сундуков
+      }
+    }
+  ]);
   console.log(accSessions)
   res.status(200).json(accSessions)
 });
